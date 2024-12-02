1ac2f470d3071beac3160e0766883e087302d4cb
arinev <arinevos@gmail.com>
2024-12-02 14:08:35 +0400

    Refactored with code for  testing in screens with different sizes

--- cypress/e2e/Tests/LoginPageTests.cy.js
+++ cypress/e2e/Tests/LoginPageTests.cy.js
@@ -3,61 +3,72 @@ import LoginPage from '../Pages/LoginPage/LoginPageActions.js';
 
 describe('Login Page View', () => {
   
-  const loginPage = new LoginPage()
+  const screenSizes = [
+      { width: 1920, height: 1080, name: 'Desktop' },
+      { width: 768, height: 1024, name: 'Tablet' },
+      { width: 375, height: 812, name: 'Mobile' },
+    ];
 
-  before(() => {  
-    loginPage.open()
-  });
-    
-  it('should display the "Foget Password" link', () => {             
-    loginPage.assertAvailability(constants.buttonFogetPassword)     
-  // Наличие всех элементов на странице в режиме полного/неполного/моб. экрана:
-  // Радио кнопка для опций эл. почты и номера тел (по умолч. на эл. почте)
- 
-  })
-  
-  it('should display radiobuttons for Email and Phone number options', () => {
-    loginPage.open()         
-    loginPage.assertElementAvailability(loginPageLocators.emailRadiobutton)
-    loginPage.assertElementAvailability(loginPageLocators.phoneRadiobutton)
-  })
+    Cypress._.each(screenSizes, (size) => {
 
-  it('should display the radiobutton for Email as checked by default', () => {
-    loginPage.open()         
-    loginPage.assertElementIsChecked(loginPageLocators.emailRadiobuttonChecked)
-  })
+      describe(`Login Page View on ${size.name} (${size.width}x${size.height})`, () => {
 
-  it('should display a welcome text', () => {
-    loginPage.open()         
-    loginPage.assertAvailability(constants.welcomeText)
-  })
+          const loginPage = new LoginPage()
 
-  it('should display the "Login" button', () => {
-    loginPage.open()         
-    loginPage.assertElementAvailability(loginPageLocators.loginButton)
-  })
-
-  it('should display the email icon for the email input field', () => {
-    loginPage.open()         
-    loginPage.assertElementAvailability(loginPageLocators.emailIcon) 
-  })
-
-  it('should display the key icon for the password input field', () => {
-    loginPage.open()         
-    loginPage.assertElementAvailability(loginPageLocators.keyIcon) 
-  })
-
-  it('should display the password input field', () => {
-    loginPage.open()         
-    loginPage.assertElementAvailability(loginPageLocators.placeholderPassswordField)
-  })
-
-  it('should display the username input field', () => {
-    loginPage.open()         
-    loginPage.assertElementAvailability(loginPageLocators.placeholderUsernameField)
-  })
- 
- 
+          beforeEach(() => {
+              // Set the viewport for the current screen size
+              cy.viewport(size.width, size.height);
+              loginPage.open();
+            });
+                         
+          it('should display the "Foget Password" link', () => {             
+            loginPage.assertAvailability(constants.buttonFogetPassword)     
+         
+          })
+          
+          it('should display radiobuttons for Email and Phone number options', () => {
+            loginPage.open()         
+            loginPage.assertElementAvailability(loginPageLocators.emailRadiobutton)
+            loginPage.assertElementAvailability(loginPageLocators.phoneRadiobutton)
+          })
+        
+          it('should display the radiobutton for Email as checked by default', () => {
+            loginPage.open()         
+            loginPage.assertElementIsChecked(loginPageLocators.emailRadiobuttonChecked)
+          })
+        
+          it('should display a welcome text', () => {
+            loginPage.open()         
+            loginPage.assertAvailability(constants.welcomeText)
+          })
+        
+          it('should display the "Login" button', () => {
+            loginPage.open()         
+            loginPage.assertElementAvailability(loginPageLocators.loginButton)
+          })
+        
+          it('should display the email icon for the email input field', () => {
+            loginPage.open()         
+            loginPage.assertElementAvailability(loginPageLocators.emailIcon) 
+          })
+        
+          it('should display the key icon for the password input field', () => {
+            loginPage.open()         
+            loginPage.assertElementAvailability(loginPageLocators.keyIcon) 
+          })
+        
+          it('should display the password input field', () => {
+            loginPage.open()         
+            loginPage.assertElementAvailability(loginPageLocators.placeholderPassswordField)
+          })
+        
+          it('should display the username input field', () => {
+            loginPage.open()         
+            loginPage.assertElementAvailability(loginPageLocators.placeholderUsernameField)
+          })
+         
+        })
+    })
 })
 
 
@@ -72,7 +83,6 @@ describe('Login Functionality', () => {
   //Add assertion of message "Поле обязательно для заполнения" для мейла
   //Add assertion of message "Поле обязательно для заполнения" для пасворда
   //Alert is closed
-  
  })
 
  it('should display errors when the username is empty', () => {
@@ -81,7 +91,6 @@ describe('Login Functionality', () => {
 
   loginPage.assertErrorAlertOpens(constants.errorAlertText)
   loginPage.assertErrorMessage(constants.errorMessageTextEmptyField)
-
  })
 
    
@@ -91,9 +100,7 @@ describe('Login Functionality', () => {
   
   loginPage.assertErrorAlertOpens(constants.errorAlertText)
   loginPage.assertErrorMessage(constants.errorMessageTextEmptyField)
-  
  })
-  
 
  it('should display errors when the password is wrong', () => {
    loginPage.open()
@@ -108,7 +115,6 @@ describe('Login Functionality', () => {
   loginPage.login(constants.wrongUsername, constants.password)
    
   loginPage.assertErrorAlertOpens(constants.errorAlertTextWrongInput)
- 
  })
 
  it('should allow login with correct credentials', () => {
@@ -117,7 +123,7 @@ describe('Login Functionality', () => {
    loginPage.login(constants.username, constants.password)
    
    loginPage.assertSuccessfulLogin(constants.userFirstname, constants.userLastname)
-  }) 
+ }) 
 
      
 
